d6f6f8598ced62c0e59e13f19f45dbbfa2db9e5b
Ivan Torres <mexpolk@gmail.com>
2011-02-11 09:09:50 -0600

    Add working screen formula

--- screen.rb
+++ screen.rb
@@ -1,28 +1,18 @@
 require 'formula'
 
-class Screen < Formula
+class Screen <Formula
   url 'http://ftp.gnu.org/gnu/screen/screen-4.0.3.tar.gz'
-  version 'v4.0.3'
+  homepage 'http://www.gnu.org/software/screen'
   md5 '8506fd205028a96c741e4037de6e3c42'
-  homepage 'http://www.gnu.org/software/screen/'
+  version '4.00.03'
 
   def patches
-    {
-      :p0 => 'https://github.com/mexpolk/homebrew-formulas/raw/master/patches/screen.patch',
-    }
+    "http://trac.macports.org/raw-attachment/ticket/20862/screen-4.0.3-snowleopard.patch"
   end
 
   def install
-    args = [
-      "--prefix=#{prefix}",
-      "--mandir=#{man}",
-      "--enable-locale",
-      "--enable-telnet",
-      "--enable-colors256",
-      "--enable-rxct_osc"
-    ]
-
-    system "./configure", *args
+    system "./configure", "--prefix=#{prefix}", "--mandir=#{man}", "--infodir=#{info}",
+                          "--enable-colors256"
     system "make"
     system "make install"
   end
