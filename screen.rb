d7d4585bacf5816f323a0caef6a4f6fd0f35d045
Ivan Torres <mexpolk@gmail.com>
2011-02-11 18:30:54 -0600

    Update GNU Screen 4.01.00devel

--- screen.rb
+++ screen.rb
@@ -1,16 +1,15 @@
 require 'formula'
 
 class Screen <Formula
-  url 'http://ftp.gnu.org/gnu/screen/screen-4.0.3.tar.gz'
   homepage 'http://www.gnu.org/software/screen'
-  md5 '8506fd205028a96c741e4037de6e3c42'
-  version '4.00.03'
+  url 'git://git.savannah.gnu.org/screen.git', :branch => 'master'
+  version '4.01.00devel'
 
-  def patches
-    { :p0 => "http://trac.macports.org/raw-attachment/ticket/20862/screen-4.0.3-snowleopard.patch" }
-  end
 
   def install
+    cd 'src'
+    system "autoconf"
+    system "autoheader"
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}", "--infodir=#{info}",
                           "--enable-colors256"
     system "make"
